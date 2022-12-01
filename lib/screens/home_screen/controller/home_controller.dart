78ff945e5d37a630a1814a45b9c3b4cefcfe3bff
Burak Şahin <buraksahin@servislet.com>
2022-12-01 13:33:11 +0300

    search news added

--- lib/screens/home_screen/controller/home_controller.dart
+++ lib/screens/home_screen/controller/home_controller.dart
@@ -1,8 +1,11 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:news_app/constants/base_urls.dart';
+import 'package:news_app/constants/general_datas.dart';
+import 'package:news_app/models/news_model/news_article_model.dart';
 import 'package:news_app/models/news_model/news_model.dart';
 import 'package:news_app/screens/home_screen/service/home_services.dart';
+import 'package:news_app/utilities/app_text_styles.dart';
 
 class HomeController extends GetxController {
   HomeServices homeServices = HomeServices();
@@ -14,22 +17,66 @@ class HomeController extends GetxController {
   late NewsModel? newsModel;
   RxBool newsLoadingStatue = false.obs;
 
-  getNewsList() async {
-    try {
-      changeLoadingStatue();
-      newsModel = await homeServices.getNewsFromService(BaseUrls.topNewsUrl);
-    } finally {
-      changeLoadingStatue();
+  RxInt currentPage = 1.obs;
+
+  List<NewsArticleModel> newsList = [];
+
+  checkTextFieldEmpty() {
+    if (seachFieldController.text == '') {
+      errorDialog('Search bar is empty!');
+    } else {
+      getNewsListwithSearch();
     }
   }
 
-  changeLoadingStatue() {
-    newsLoadingStatue.value = !newsLoadingStatue.value;
+  getNewsListwithSearch() async {
+    try {
+      changeLoadingStatue(newsLoadingStatue);
+
+      newsModel = await homeServices.getNewsFromService(seachFieldController
+                  .text ==
+              ''
+          ? "${BaseUrls.topNewsUrl}&page=$currentPage&pageSize=10"
+          : "${BaseUrls.baseUrl}everything?q=${seachFieldController.text}&apiKey=${GeneralData.apiKey}&page=$currentPage&pageSize=10");
+
+      newsList = newsModel!.articles!;
+      currentPage.value++;
+    } finally {
+      changeLoadingStatue(newsLoadingStatue);
+    }
+  }
+
+  changeLoadingStatue(RxBool loadingStatue) {
+    loadingStatue.value = !loadingStatue.value;
+  }
+
+  errorDialog(String content) {
+    return Get.defaultDialog(
+        title: 'Warning',
+        barrierDismissible: false,
+        content: Text(
+          content,
+        ),
+        actions: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+            ),
+            onPressed: () {
+              Get.back();
+            },
+            child: Text(
+              "OK",
+              style: AppTextStyles.boldTextStyle,
+            ),
+          )
+        ]);
   }
 
   @override
   void onInit() {
     super.onInit();
-    getNewsList();
+    getNewsListwithSearch();
   }
 }
